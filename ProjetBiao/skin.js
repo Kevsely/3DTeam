69d698c944ab8b4ca76fc74813c24c8a1264c74a
Kevsely <elegis.sossou@yahoo.com>
2022-04-25 17:15:03 +0100

    ok

--- ProjetBiao/skin.js
+++ ProjetBiao/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.2/17253
 // Filename: Vide 3D TEAM ss logo.ggsk
-// Generated sam. avr. 23 03:51:52 2022
+// Generated lun. avr. 25 17:05:08 2022
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_3d_preview', 2, true);
