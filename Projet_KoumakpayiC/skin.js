6aeaa278478c4667394edb74fa60f6659787c0ae
Kevsely <elegis.sossou@yahoo.com>
2022-07-21 11:51:08 +0100

    ok

--- Projet_KoumakpayiC/skin.js
+++ Projet_KoumakpayiC/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.2/17253
 // Filename: Vide 3D TEAM ss logo.ggsk
-// Generated mar. juil. 19 23:51:23 2022
+// Generated jeu. juil. 21 11:49:31 2022
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_3d_preview', 2, true);
